901706b1592c9a59b38fa8640362cff260dde915
Terry Filiba <tfiliba@gmail.com>
2013-03-29 16:42:58 -0700

    Moving files based on technology

--- fpga/fft/copy_results.py
+++ fpga/fft/copy_results.py
@@ -1,8 +1,29 @@
 import glob
 import shutil
+import re
+import os.path
+
+
+blockname = 'fft'
+technologyindex = 5
+
+#for directory in glob.iglob('')
+technames = set([])
+for builddirectory in glob.iglob(blockname+'*/'):
+    technames.add(re.split('/|_',builddirectory)[technologyindex])
+    
+
+for techname in list(technames):
+#    print techname
+    techdir = os.path.dirname('results/'+techname+'/')
+    if not os.path.exists(techdir):
+        os.makedirs(techdir)
 
 for file in glob.iglob('*/sysgen/xflow/*_map.map'):
-    shutil.copy(file,'results')
+    techname = re.split('/|_',file)[technologyindex]
+    shutil.copy(file,'results/'+techname+'/')
+    
 for file in glob.iglob('*/sysgen/xflow/*.twr'):
-    shutil.copy(file,'results')
+    techname = re.split('/|_',file)[technologyindex]
+    shutil.copy(file,'results/'+techname+'/')
 
